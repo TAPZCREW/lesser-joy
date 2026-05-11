97e91a0e63628c85a4a51604cb8f52cf92ba257c
Maxime Trimolet <maxime.trimolet@gmail.com>
2026-05-11 18:37:44 +0200

    detect wdk dir from installed package
    WIP: first it fails when running xmake config once,
    then it seems to succeed when running xmake config once more.

--- xmake.lua
+++ xmake.lua
@@ -1,25 +1,51 @@
 rule("install-wdk", function()
-    on_load(function()
+    add_imports("core.project.config")
+
+    on_load(function(target)
         import("lib.detect.find_tool")
 
-        local nuget = find_tool("nuget", { check = function(tool) os.run("%s help", tool) end })
-        assert(nuget, "nuget not found!")
+        if config.get("wdk") then
+          return
+        end
 
-        if not os.isdir("packages") then
+        local nuget_packages_dir = path.join(os.projectdir(), "packages")
+
+        function find_dir(pattern)
+            for _, dir in ipairs(os.dirs(pattern)) do
+                return dir
+            end
+            return nil
+        end
+
+        local wdk_package_id = "Microsoft.Windows.WDK." .. (target:is_arch("arm64") and "arm64" or "x64")
+        local wdk_package_dir = find_dir(path.join(nuget_packages_dir, wdk_package_id) .. ".*")
+
+        if not wdk_package_dir then
+            local nuget = find_tool("nuget", { check = function(tool) os.run("%s help", tool) end })
+            assert(nuget, "nuget not found!")
+
             print("installing wdk")
             os.execv(nuget.program, {
                 "restore",
                 path.join(os.projectdir(), "packages.config"),
                 "-PackagesDirectory",
-                path.join(os.projectdir(), "packages"),
+                nuget_packages_dir,
+                "-Verbosity",
+                "quiet"
             }, {})
+
+            wdk_package_dir = find_dir(path.join(nuget_packages_dir, wdk_package_id) .. ".*")
+            assert(wdk_package_dir, "WDK package not found!")
         end
+
+        target:add("values", "wdk", wdk_package_dir)
     end)
 end)
 
 add_rules("mode.debug", "mode.release")
 
 add_rules("install-wdk")
+
 includes("stormkit-driver/xmake.lua")
 includes("driver/xmake.lua")
 includes("qt-frontend/xmake.lua")
