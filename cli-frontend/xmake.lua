935e719b68b4501915b86ce033dfd0fb554b1830
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-06-27 16:13:56 +0200

    add stormkit dep for lesser-joy cli

--- cli-frontend/xmake.lua
+++ cli-frontend/xmake.lua
@@ -1,5 +1,17 @@
-target("lesser-joy-cli", function()
+target("lesserjoy-cli", function()
     set_languages("c++26")
     add_files("src/*.cpp")
     set_policy("build.c++.modules", true)
+
+    set_runtimes("c++_static")
+
+    add_rules(stormkit_rule_prefix .. "stormkit::application")
+    set_values("stormkit.components", { "stormkit", "log" })
+
+    add_syslinks("OneCoreUAP")
+
+    on_run(function(target)
+        import("privilege.sudo")
+        sudo.exec(target:targetfile())
+    end)
 end)
